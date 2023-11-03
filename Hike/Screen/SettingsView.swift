d0b8aaca457075e7aeab0d35d263357bba1cffc7
Kasthuri <tkasthu.90@gmail.com>
2023-11-03 14:48:00 +1100

    Implement alternate app icons part

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -8,7 +8,17 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // Properties
+    private let alternateAppIcons: [String] = [
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Map",
+        "AppIcon-Mushroom",
+        "AppIcon-Camera",
+        "AppIcon-Backpack",
+        "AppIcon-Campfire"
+    ]
     var body: some View {
+        
         List{
             // header
             Section{
@@ -47,46 +57,85 @@ struct SettingsView: View {
                 Text("Dust off the boots! It's time for a walk.")
                     .fontWeight(.heavy)
                     .foregroundColor(.customGreenMedium)
+                
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 16)
+                    .frame(maxWidth: .infinity)
+            } //header
+            .listRowSeparator(.hidden)
+            // Icons
+            Section(header: Text("Alternate Icons")){
+                // Icons
+                ScrollView(.horizontal, showsIndicators: false)
+                {
+                    HStack{
+                        ForEach(alternateAppIcons.indices, id:\.self){
+                            item in
+                            
+                            Button {
+                                print("Icon \(alternateAppIcons[item]) was pressed.")
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]){ error in
+                                    if error != nil{
+                                        
+                                        print("Failed request to update the app's icon: \(String(describing:error?.localizedDescription))")
+                                        // by default changing iconsis not supported, to enable go to soln1: Hike -> target -> General> enable app icons source
+                                        //Soln2: Hike -> target -> Build settings -> alternate app icon set -> add all the icon name there
+                                    }
+                                    else{
+                                        print("You have changed the app's icon to \(alternateAppIcons[item])")
+                                    }
+                                }
+                            }label: {
+                                Image("\(alternateAppIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(16)
+                            }
+                            
+                            
+                            .buttonStyle(.borderless)
+                        }
+                    }
+                } .padding(.top,12)
+                Text("Choose your favorite icon from the collection above")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+            }.listRowSeparator(.hidden)
+            // About
+            Section(
+                header: Text("About the App"),
+                footer: HStack{
+                    Spacer()
+                    Text("Copyright © All right reserved.")
+                    Spacer()
+                }.padding(.vertical,8)
+            )
+            {
+                //1 .Basic labeled content
+                //  LabeledContent("Application", value: "Hike")
+                // 2 . Advance labeled content
+                
+                //Reusable ui component - customlistrowview
+                CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "HIKE", rowTintColor: .blue)
+                
+                CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "iOS, iPadOS", rowTintColor: .red)
+                
+                CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+                
+                CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+                
+                CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "John Doe", rowTintColor: .mint)
+                
+                CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
+                
+                CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Academy", rowLinkDestination: "https://credo.academy")
             }
-            .multilineTextAlignment(.center)
-            .padding(.bottom, 16)
-            .frame(maxWidth: .infinity)
-        } //header
-        .listRowSeparator(.hidden)
-        // Icons
-        
-        // About
-        Section(
-            header: Text("About the App"),
-            footer: HStack{
-                Spacer()
-                Text("Copyright © All right reserved.")
-                Spacer()
-            }.padding(.vertical,8)
-        )
-        {
-            //1 .Basic labeled content
-            //  LabeledContent("Application", value: "Hike")
-            // 2 . Advance labeled content
-            
-            //Reusable ui component - customlistrowview
-            CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "HIKE", rowTintColor: .blue)
-            
-            CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "iOS, iPadOS", rowTintColor: .red)
-            
-            CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
-            
-            CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
-            
-            CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "John Doe", rowTintColor: .mint)
-            
-            CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
-            
-            CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Acedemy", rowLinkDestination: "https://credo.academy")
         }
     }
 }
-
 #Preview {
     SettingsView()
 }
